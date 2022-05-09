6809d81f8a46755197995626bbfdd633ccf0dcea
Onnwen <onnwen.cassitto@icloud.com>
2022-05-09 09:12:17 +0200

    testing

--- api/login.php
+++ api/login.php
@@ -6,16 +6,18 @@ header("Access-Control-Allow-Origin: *");
 header("Content-type: application/json");
 
 $con = mysqli_connect('52.47.52.89','on','passwordSegretaDatabase','gioco');
+
+if(mysqli_connect_errno()) {
+    $msg = "Database connection failed: ";
+    $msg .= mysqli_connect_error();
+    $msg .= " : " . mysqli_connect_errno();
+    exit($msg);
+}
+
 $sql = "SELECT * FROM `utenti` WHERE `username` = '".$username."' AND `password` = '".$password."'";
 
 $res = mysqli_query($con,$sql);
 
-if (res) {
-    echo("Connected!");
-}
-else {
-    echo("Connection failed.");
-}
 $array = mysqli_fetch_array($res);
 $result = array();
 
